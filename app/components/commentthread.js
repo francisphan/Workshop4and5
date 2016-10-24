d04103bc56f04c751567f7e9457a6feb6f870f5d
Francis Phan <fphan@umass.edu>
2016-10-24 14:59:14 -0400

    fb3

---
+++ app/components/commentthread.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import CommentEntry from './commententry';
+
+export default class CommentThread extends React.Component {
+  render() {
+    return (
+      <ul className="media-list">
+        {React.Children.map(this.props.children, function(child) {
+          return (
+            <li className="media">
+              {child}
+            </li>
+          )
+        })}
+      </ul>
+    )
+  }
+}
